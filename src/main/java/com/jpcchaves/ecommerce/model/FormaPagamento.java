76b4a9f84e08609e48d315fa4ce06dfc7a286e9b
João Chaves <joao.chaves@liferay.com>
2024-06-15 15:10:05 -0300

    add forma pagamento model

---
+++ src/main/java/com/jpcchaves/ecommerce/model/FormaPagamento.java
@@ -0,0 +1,67 @@
+package com.jpcchaves.ecommerce.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "forma_pagamento")
+@SequenceGenerator(
+    name = "seq_forma_pagamento",
+    sequenceName = "seq_forma_pagamento",
+    allocationSize = 1)
+public class FormaPagamento implements Serializable {
+  private static final long serialVersionUID = 2592810559106621945L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_forma_pagamento")
+  private Long id;
+
+  private String descricao;
+
+  public FormaPagamento() {}
+
+  public FormaPagamento(Long id, String descricao) {
+    this.id = id;
+    this.descricao = descricao;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getDescricao() {
+    return descricao;
+  }
+
+  public void setDescricao(String descricao) {
+    this.descricao = descricao;
+  }
+
+  @Override
+  public String toString() {
+    return "FormaPagamento{" + "id=" + id + ", descricao='" + descricao + '\'' + '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    FormaPagamento that = (FormaPagamento) o;
+    return Objects.equals(id, that.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
+}
