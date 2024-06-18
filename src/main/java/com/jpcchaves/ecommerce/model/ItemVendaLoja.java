8a67dfcf57b22dd44066384cd99afd7428284319
Joao Chaves <joao.chaves@liferay.com>
2024-06-18 16:30:46 -0300

    add item venda loja model

---
+++ src/main/java/com/jpcchaves/ecommerce/model/ItemVendaLoja.java
@@ -0,0 +1,77 @@
+package com.jpcchaves.ecommerce.model;
+
+import java.io.*;
+import java.util.*;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "item_venda_loja")
+@SequenceGenerator(
+    name = "seq_item_venda_loja",
+    sequenceName = "seq_item_venda_loja",
+    allocationSize = 1)
+public class ItemVendaLoja implements Serializable {
+  private static final long serialVersionUID = -8092255183897708225L;
+
+  @Id
+  @GeneratedValue(
+      strategy = GenerationType.SEQUENCE,
+      generator = "seq_item_venda_loja")
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(
+      name = "produto_id",
+      nullable = false,
+      foreignKey =
+          @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "produto_fk"))
+  private Produto produto;
+
+  @ManyToOne
+  @JoinColumn(
+      name = "venda_compra_loja_virtual_id",
+      nullable = false,
+      foreignKey =
+          @ForeignKey(
+              value = ConstraintMode.CONSTRAINT,
+              name = "venda_compra_loja_virtual_fk"))
+  private VendaCompraLojaVirtual vendaCompraLojaVirtual;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Produto getProduto() {
+    return produto;
+  }
+
+  public void setProduto(Produto produto) {
+    this.produto = produto;
+  }
+
+  public VendaCompraLojaVirtual getVendaCompraLojaVirtual() {
+    return vendaCompraLojaVirtual;
+  }
+
+  public void setVendaCompraLojaVirtual(
+      VendaCompraLojaVirtual vendaCompraLojaVirtual) {
+    this.vendaCompraLojaVirtual = vendaCompraLojaVirtual;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    ItemVendaLoja that = (ItemVendaLoja) o;
+    return Objects.equals(id, that.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
+}
