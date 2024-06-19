4615b381c92dfd4d78762fd002798f804dafb4c0
Joao Chaves <joao.chaves@liferay.com>
2024-06-19 16:27:40 -0300

    add not null columns in endereco model

--- src/main/java/com/jpcchaves/ecommerce/model/Endereco.java
+++ src/main/java/com/jpcchaves/ecommerce/model/Endereco.java
@@ -7,29 +7,48 @@ import javax.persistence.*;
 
 @Entity
 @Table(name = "endereco")
-@SequenceGenerator(name = "seq_endereco", sequenceName = "seq_endereco", allocationSize = 1)
+@SequenceGenerator(
+    name = "seq_endereco",
+    sequenceName = "seq_endereco",
+    allocationSize = 1)
 public class Endereco implements Serializable {
   private static final long serialVersionUID = -1963101071500820594L;
 
   @Id
-  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_endereco")
+  @GeneratedValue(
+      strategy = GenerationType.SEQUENCE,
+      generator = "seq_endereco")
   private Long id;
 
+  @Column(nullable = false)
   private String ruaLogradouro;
+
+  @Column(nullable = false)
   private String cep;
+
+  @Column(nullable = false)
   private String numero;
+
   private String complemento;
+
+  @Column(nullable = false)
   private String bairro;
+
+  @Column(nullable = false)
   private String uf;
+
+  @Column(nullable = false)
   private String cidade;
 
   @ManyToOne(targetEntity = Pessoa.class)
   @JoinColumn(
       name = "pessoa_id",
       nullable = false,
-      foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
+      foreignKey =
+          @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
   private Pessoa pessoa;
 
+  @Column(nullable = false)
   @Enumerated(EnumType.STRING)
   private TipoEndereco tipoEndereco;
 
