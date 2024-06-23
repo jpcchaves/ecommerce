ead946eb4523265a3c8e24efa18e97975c9d1be4
João Chaves <joao.chaves@liferay.com>
2024-06-23 11:38:25 -0300

    add jpa annotations

--- src/main/java/com/jpcchaves/ecommerce/EcommerceApplication.java
+++ src/main/java/com/jpcchaves/ecommerce/EcommerceApplication.java
@@ -2,8 +2,12 @@ package com.jpcchaves.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = {"com.jpcchaves.ecommerce.repository"})
+@EnableTransactionManagement
 public class EcommerceApplication {
 
 	public static void main(String[] args) {
