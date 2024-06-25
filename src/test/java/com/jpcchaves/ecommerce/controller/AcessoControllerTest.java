cd9ff068e2d1a5fb89d7c6d742b576955da51df1
Joao Chaves <joao.chaves@liferay.com>
2024-06-25 11:01:44 -0300

    add test to create, find by id, find by desc, delete by id

--- src/test/java/com/jpcchaves/ecommerce/controller/AcessoControllerTest.java
+++ src/test/java/com/jpcchaves/ecommerce/controller/AcessoControllerTest.java
@@ -1,16 +1,20 @@
 package com.jpcchaves.ecommerce.controller;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import com.jpcchaves.ecommerce.model.*;
+import com.jpcchaves.ecommerce.repository.*;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.boot.test.context.*;
 
-import com.jpcchaves.ecommerce.model.Acesso;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class AcessoControllerTest {
 
+  @Autowired
+  AcessoRepository acessoRepository;
   @Autowired
   private AcessoController acessoController;
 
@@ -20,12 +24,48 @@ public class AcessoControllerTest {
 
     acesso.setDescricao("ROLE_TEST");
 
-    acesso = acessoController.salvarAcesso(acesso).getBody();
+    acesso = acessoController.salvarAcesso(acesso)
+                             .getBody();
 
     // assert if the object returned by the database isn't null
     assertNotNull(acesso);
 
-    // assert if the ID was generated successfully and the value are greater than 0
+    // assert if the ID was generated successfully and the value are greater
+    // than 0
     assertTrue(acesso.getId() > 0);
+
+
+    // Fetch the created accesso
+    Acesso createdAcesso = acessoRepository.findById(acesso.getId())
+                                           .get();
+    assertNotNull(createdAcesso);
+
+    assertEquals(acesso.getId(), createdAcesso.getId());
+
+    // Delete the created acesso
+    acessoRepository.deleteById(createdAcesso.getId());
+
+    // Update DB
+    acessoRepository.flush();
+
+    Acesso deletedAcesso = acessoRepository.findById(createdAcesso.getId())
+                                           .orElse(null);
+
+    assertNull(deletedAcesso);
+
+    // Query
+    acesso = new Acesso();
+
+    acesso.setDescricao("ROLE_ALUNO");
+
+    acesso = acessoController.salvarAcesso(acesso)
+                             .getBody();
+
+    List<Acesso> acessosList = acessoRepository.findAcessoByDescricao("ALUNO".trim()
+                                                                             .toUpperCase());
+    assertNotNull(acesso);
+    assertEquals(1, acessosList.size());
+
+    acessoRepository.deleteById(acesso.getId());
   }
 }
