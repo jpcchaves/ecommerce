e8fdf090f92d59958aa88c98c56a75fbce873458
Joao Chaves <joao.chaves@liferay.com>
2024-06-25 11:01:21 -0300

    add custom query to find by desc

--- src/main/java/com/jpcchaves/ecommerce/repository/AcessoRepository.java
+++ src/main/java/com/jpcchaves/ecommerce/repository/AcessoRepository.java
@@ -1,13 +1,17 @@
 package com.jpcchaves.ecommerce.repository;
 
-import com.jpcchaves.ecommerce.model.Acesso;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+import com.jpcchaves.ecommerce.model.*;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.*;
+import org.springframework.transaction.annotation.*;
+
+import java.util.*;
 
 @Repository
 @Transactional
 public interface AcessoRepository extends
     JpaRepository<Acesso, Long> {
 
+  @Query("select a from Acesso a where upper(trim(a.descricao)) like %?1%")
+  List<Acesso> findAcessoByDescricao(String descricao);
 }
