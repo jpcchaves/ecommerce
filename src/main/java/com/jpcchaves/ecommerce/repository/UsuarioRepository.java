652b1086acd949e4236dfff4155386643f687155
João Chaves <joao.chaves@liferay.com>
2024-06-29 15:12:51 -0300

    make findUserByUsername return optional

--- src/main/java/com/jpcchaves/ecommerce/repository/UsuarioRepository.java
+++ src/main/java/com/jpcchaves/ecommerce/repository/UsuarioRepository.java
@@ -1,6 +1,7 @@
 package com.jpcchaves.ecommerce.repository;
 
 import com.jpcchaves.ecommerce.model.Usuario;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -9,5 +10,5 @@ import org.springframework.stereotype.Repository;
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
   @Query(value = "select u from Usuario where u.login = ?1")
-  Usuario finsUserByLogin(String login);
+  Optional<Usuario> finsUserByLogin(String login);
 }
