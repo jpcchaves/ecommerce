1ea08f80aa3dad5d16fbdbc5503399b0ac860dbb
João Chaves <joao.chaves@liferay.com>
2024-06-29 15:16:01 -0300

    implemented configure method for authentication manager builder

--- src/main/java/com/jpcchaves/ecommerce/security/WebConfigSecurity.java
+++ src/main/java/com/jpcchaves/ecommerce/security/WebConfigSecurity.java
@@ -3,16 +3,31 @@ package com.jpcchaves.ecommerce.security;
 import javax.servlet.http.HttpSessionListener;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class WebConfigSecurity extends WebSecurityConfigurerAdapter implements
     HttpSessionListener {
+
+  private final UserDetailsService userDetailsService;
+
+  public WebConfigSecurity(UserDetailsService userDetailsService) {
+    this.userDetailsService = userDetailsService;
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userDetailsService)
+        .passwordEncoder(new BCryptPasswordEncoder());
+  }
 
   @Override
   public void configure(WebSecurity web) throws Exception {
