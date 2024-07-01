6ba1ffe3ec5dcc90ccde957eea5cf3b968f22478
João Chaves <joao.chaves@liferay.com>
2024-06-30 21:12:44 -0300

    add jwt filter

---
+++ src/main/java/com/jpcchaves/ecommerce/security/JwtApiAuthenticacaoFilter.java
@@ -0,0 +1,31 @@
+package com.jpcchaves.ecommerce.security;
+
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+public class JwtApiAuthenticacaoFilter extends GenericFilterBean {
+
+  @Override
+  public void doFilter(
+      ServletRequest servletRequest,
+      ServletResponse servletResponse,
+      FilterChain filterChain
+  ) throws IOException, ServletException {
+
+    Authentication authentication = new JwtTokenAuthenticationService().getAuthentication(
+        (HttpServletRequest) servletRequest,
+        (HttpServletResponse) servletResponse);
+
+    SecurityContextHolder.getContext().setAuthentication(authentication);
+
+    filterChain.doFilter(servletRequest, servletResponse);
+  }
+}
