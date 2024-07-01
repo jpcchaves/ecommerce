8b02ac60635a1035aa243bd026ab4c2b5f483446
Joao Chaves <joao.chaves@liferay.com>
2024-07-01 08:47:05 -0300

    handle signature exception

--- src/main/java/com/jpcchaves/ecommerce/security/JwtTokenAuthenticationService.java
+++ src/main/java/com/jpcchaves/ecommerce/security/JwtTokenAuthenticationService.java
@@ -1,40 +1,40 @@
 package com.jpcchaves.ecommerce.security;
 
-import com.jpcchaves.ecommerce.config.ApplicationContextConfig;
-import com.jpcchaves.ecommerce.model.Usuario;
-import com.jpcchaves.ecommerce.repository.UsuarioRepository;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import java.util.Date;
-import java.util.Objects;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
+import com.jpcchaves.ecommerce.config.*;
+import com.jpcchaves.ecommerce.model.*;
+import com.jpcchaves.ecommerce.repository.*;
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.security.authentication.*;
+import org.springframework.security.core.*;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.*;
+
+import javax.servlet.http.*;
+import java.io.*;
+import java.util.*;
 
 @Service
 public class JwtTokenAuthenticationService {
 
   private static final long EXPIRATION_TIME_30D = 2592000000L;
-
+  private static final String TOKEN_PREFIX = "Bearer";
+  private static final String HEADER_AUTHORIZATION = "Authorization";
   @Value("${application.security.jwt.secret-key}")
   private static String SECRET_KEY;
 
-  private static final String TOKEN_PREFIX = "Bearer";
-  private static final String HEADER_AUTHORIZATION = "Authorization";
-
-  public void addAuthentication(HttpServletResponse response, String username)
+  public void addAuthentication(
+      HttpServletResponse response,
+      String username
+  )
       throws Exception {
 
     String jwt = Jwts.builder()
-        .setSubject(username)
-        .setExpiration(
-            new Date(System.currentTimeMillis() + EXPIRATION_TIME_30D))
-        .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
-        .compact();
+                     .setSubject(username)
+                     .setExpiration(
+                         new Date(System.currentTimeMillis() + EXPIRATION_TIME_30D))
+                     .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+                     .compact();
 
     String token = TOKEN_PREFIX + " " + jwt;
 
@@ -42,38 +42,49 @@ public class JwtTokenAuthenticationService {
 
     configureCors(response);
 
-    response.getWriter().write("{\"Authorization\": \"" + token + "\" }");
+    response.getWriter()
+            .write("{\"Authorization\": \"" + token + "\" }");
 
   }
 
   public Authentication getAuthentication(
       HttpServletRequest request,
       HttpServletResponse response
-  ) {
+  ) throws IOException {
 
     String authorizationHeader = request.getHeader(HEADER_AUTHORIZATION);
 
-    if (!Objects.isNull(authorizationHeader)) {
+    try {
+      if (!Objects.isNull(authorizationHeader)) {
 
-      String token = authorizationHeader.replace(TOKEN_PREFIX, "").trim();
+        String token = authorizationHeader.replace(TOKEN_PREFIX, "")
+                                          .trim();
 
-      String user = Jwts.parser().setSigningKey(SECRET_KEY)
-          .parseClaimsJws(token).getSignature();
+        String user = Jwts.parser()
+                          .setSigningKey(SECRET_KEY)
+                          .parseClaimsJws(token)
+                          .getSignature();
 
-      if (!Objects.isNull(user)) {
+        if (!Objects.isNull(user)) {
 
-        Usuario usuario = ApplicationContextConfig.getApplicationContext()
-            .getBean(
-                UsuarioRepository.class).finsUserByLogin(user)
-            .orElseThrow(() -> new UsernameNotFoundException(""));
+          Usuario usuario = ApplicationContextConfig.getApplicationContext()
+                                                    .getBean(
+                                                        UsuarioRepository.class)
+                                                    .finsUserByLogin(user)
+                                                    .orElseThrow(() -> new UsernameNotFoundException(""));
 
-        return new UsernamePasswordAuthenticationToken(usuario,
-            usuario.getPassword(), usuario.getAuthorities());
+          return new UsernamePasswordAuthenticationToken(usuario,
+                                                         usuario.getPassword(), usuario.getAuthorities());
+        }
       }
+
+    } catch (SignatureException exception) {
+      response.getWriter()
+              .write("Invalid Token!");
+    } finally {
+      configureCors(response);
     }
 
-    configureCors(response);
-    
     return null;
   }
 
