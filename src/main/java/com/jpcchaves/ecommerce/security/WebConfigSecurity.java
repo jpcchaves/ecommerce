b1e1c2941c9b4bb7b03b9461d1b32c6f7a1a68a6
João Chaves <joao.chaves@liferay.com>
2024-06-30 21:12:44 -0300

    add jwt filter

--- src/main/java/com/jpcchaves/ecommerce/security/WebConfigSecurity.java
+++ src/main/java/com/jpcchaves/ecommerce/security/WebConfigSecurity.java
@@ -5,11 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -24,13 +27,33 @@ public class WebConfigSecurity extends WebSecurityConfigurerAdapter implements
   }
 
   @Override
-  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.userDetailsService(userDetailsService)
-        .passwordEncoder(new BCryptPasswordEncoder());
+  protected void configure(HttpSecurity http) throws Exception {
+
+    http.csrf()
+        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+        .disable()
+        .authorizeRequests()
+        .antMatchers("/").permitAll()
+        .antMatchers("/index").permitAll()
+        .antMatchers(HttpMethod.OPTIONS, "/**")
+        .permitAll()
+        .anyRequest()
+        .authenticated()
+        .and()
+        .logout()
+        .logoutSuccessUrl("/index")
+        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+        .and()
+        .addFilterAfter(new JwtLoginFilter("/login", authenticationManager()),
+            UsernamePasswordAuthenticationFilter.class)
+        .addFilterBefore(new JwtApiAuthenticacaoFilter(),
+            UsernamePasswordAuthenticationFilter.class);
+
   }
 
   @Override
-  public void configure(WebSecurity web) throws Exception {
-    web.ignoring().antMatchers(HttpMethod.POST, "/api/v1/acessos");
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userDetailsService)
+        .passwordEncoder(new BCryptPasswordEncoder());
   }
 }
