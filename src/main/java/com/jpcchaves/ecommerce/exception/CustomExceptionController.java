3518ecf6511fd0a62b58160af2c3fbdba2eee19d
Joao Chaves <joao.chaves@liferay.com>
2024-07-02 08:24:35 -0300

    add common database exceptions handling

--- src/main/java/com/jpcchaves/ecommerce/exception/CustomExceptionController.java
+++ src/main/java/com/jpcchaves/ecommerce/exception/CustomExceptionController.java
@@ -1,6 +1,8 @@
 package com.jpcchaves.ecommerce.exception;
 
 import com.jpcchaves.ecommerce.model.dto.*;
+import org.hibernate.exception.*;
+import org.springframework.dao.*;
 import org.springframework.http.*;
 import org.springframework.validation.*;
 import org.springframework.web.bind.*;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.*;
 import org.springframework.web.servlet.mvc.method.annotation.*;
 
+import java.sql.*;
 import java.util.*;
 
 @RestControllerAdvice
@@ -44,6 +47,37 @@ public class CustomExceptionController extends ResponseEntityExceptionHandler {
 
     exceptionResponseDTO.setCode(status.value() + "==>" + status.getReasonPhrase());
     exceptionResponseDTO.setError(errorMsg.toString());
+
+    return new ResponseEntity<>(exceptionResponseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  // Database exceptions
+  @ExceptionHandler({DataIntegrityViolationException.class, ConstraintViolationException.class, SQLException.class,})
+  protected ResponseEntity<Object> handleExceptionDataIntegrity(Exception ex) {
+
+    ExceptionResponseDTO exceptionResponseDTO = new ExceptionResponseDTO();
+
+    StringBuilder errorMsg = new StringBuilder();
+
+    if (ex instanceof DataIntegrityViolationException) {
+
+      errorMsg.append(ex.getCause()
+                        .getMessage());
+
+    } else if (ex instanceof ConstraintViolationException) {
+
+      errorMsg.append(ex.getCause()
+                        .getMessage());
+
+    } else if (ex instanceof SQLException) {
+
+      errorMsg.append(ex.getCause()
+                        .getMessage());
+
+    }
+
+    exceptionResponseDTO.setError(errorMsg.toString());
+    exceptionResponseDTO.setCode(HttpStatus.INTERNAL_SERVER_ERROR.toString());
 
     return new ResponseEntity<>(exceptionResponseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
   }
