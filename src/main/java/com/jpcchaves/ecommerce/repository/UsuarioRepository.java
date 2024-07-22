afdbeed9be27ce39d255f00aa2e2360c777b0c48
Joao Chaves <joao.chaves@liferay.com>
2024-07-22 14:24:25 -0300

    add custom methods to usuario repository

--- src/main/java/com/jpcchaves/ecommerce/repository/UsuarioRepository.java
+++ src/main/java/com/jpcchaves/ecommerce/repository/UsuarioRepository.java
@@ -1,14 +1,36 @@
 package com.jpcchaves.ecommerce.repository;
 
 import com.jpcchaves.ecommerce.model.Usuario;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
-  @Query(value = "SELECT * FROM Usuario u WHERE u.login = :login", nativeQuery = true)
+  @Query(value = "SELECT * FROM Usuario u WHERE u.login = :login",
+      nativeQuery = true)
   Optional<Usuario> finsUserByLogin(String login);
+
+  @Query(
+      value = "select u from Usuario u where u.pessoa.id = ?1 or u.login = ?2"
+  )
+  Usuario findByPessoa(
+      Long id,
+      String email
+  );
+
+  @Modifying
+  @Transactional
+  @Query(
+      nativeQuery = true,
+      value = "insert into usuarios_acesso(usuario_id, acesso_id) values (?1," +
+          " " +
+          "(select id from acesso where descricao = 'ROLE_USER'))"
+  )
+  void insereAcessoUserPj(Long idUser);
 }
