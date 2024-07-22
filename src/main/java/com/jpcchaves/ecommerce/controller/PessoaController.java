b61f6ab110b355edcbd7af35a74dfc2b3c61c531
Joao Chaves <joao.chaves@liferay.com>
2024-07-22 14:25:07 -0300

    add salvar pj endpoint

---
+++ src/main/java/com/jpcchaves/ecommerce/controller/PessoaController.java
@@ -0,0 +1,28 @@
+package com.jpcchaves.ecommerce.controller;
+
+import com.jpcchaves.ecommerce.model.PessoaJuridica;
+import com.jpcchaves.ecommerce.service.PessoaService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/pessoas")
+public class PessoaController {
+
+  private final PessoaService pessoaService;
+
+  public PessoaController(PessoaService pessoaService) {
+    this.pessoaService = pessoaService;
+  }
+
+  @PostMapping("/pj")
+  public ResponseEntity<PessoaJuridica> salvarPJ(@RequestBody PessoaJuridica pessoaJuridica) {
+
+    return ResponseEntity.status(HttpStatus.CREATED)
+                         .body(pessoaService.salvarPJ(pessoaJuridica));
+  }
+}
