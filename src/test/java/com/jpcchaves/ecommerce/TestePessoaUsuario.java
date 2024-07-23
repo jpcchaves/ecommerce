5b4ac0fece7c39453ef14071d533787e7a535d96
Joao Chaves <joao.chaves@liferay.com>
2024-07-23 16:46:49 -0300

    adjust unit test create pessoa usuario to also create it with endereco

--- src/test/java/com/jpcchaves/ecommerce/TestePessoaUsuario.java
+++ src/test/java/com/jpcchaves/ecommerce/TestePessoaUsuario.java
@@ -1,6 +1,8 @@
 package com.jpcchaves.ecommerce;
 
 import com.jpcchaves.ecommerce.controller.PessoaController;
+import com.jpcchaves.ecommerce.enums.TipoEndereco;
+import com.jpcchaves.ecommerce.model.Endereco;
 import com.jpcchaves.ecommerce.model.PessoaJuridica;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
 
 import java.util.Calendar;
+import java.util.List;
 
 @Profile("test")
 @SpringBootTest
@@ -31,6 +34,14 @@ public class TestePessoaUsuario {
     pessoaJuridica.setInscEstadual("81999999999");
     pessoaJuridica.setNomeFanstasia("Nome Fantasia Test");
     pessoaJuridica.setRazaoSocial("Razao Social");
+
+    pessoaJuridica.setEnderecos(
+        List.of(
+            new Endereco("Rua logradouro", "55000000", "345", "complemento",
+                         "bairro tal", "PE", "Caruaru", pessoaJuridica,
+                         TipoEndereco.COBRANCA, pessoaJuridica)
+        )
+    );
 
     pessoaController.salvarPJ(pessoaJuridica);
   }
