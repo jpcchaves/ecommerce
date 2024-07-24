724374bd6c5a437c02f903636c579e2dc4a93371
Joao Chaves <joao.chaves@liferay.com>
2024-07-24 08:49:57 -0300

    add mail configuration async

--- src/main/java/com/jpcchaves/ecommerce/EcommerceApplication.java
+++ src/main/java/com/jpcchaves/ecommerce/EcommerceApplication.java
@@ -3,15 +3,17 @@ package com.jpcchaves.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableAsync
 @EnableJpaRepositories(basePackages = {"com.jpcchaves.ecommerce.repository"})
 @EnableTransactionManagement
 public class EcommerceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EcommerceApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(EcommerceApplication.class, args);
+  }
 
 }
---
+++ src/main/java/com/jpcchaves/ecommerce/config/AsyncConfigurerConfig.java
@@ -0,0 +1,28 @@
+package com.jpcchaves.ecommerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+public class AsyncConfigurerConfig implements AsyncConfigurer {
+
+  @Override
+  @Bean
+  public Executor getAsyncExecutor() {
+
+    ThreadPoolTaskExecutor threadPoolExecutor = new ThreadPoolTaskExecutor();
+
+    threadPoolExecutor.setCorePoolSize(10);
+    threadPoolExecutor.setMaxPoolSize(20);
+    threadPoolExecutor.setQueueCapacity(500);
+    threadPoolExecutor.setThreadNamePrefix("Async Thread");
+
+    threadPoolExecutor.initialize();
+
+    return threadPoolExecutor;
+  }
+}
