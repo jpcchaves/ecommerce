ab19971a64da4e2b9c5f39d403f09dbb8e19db6d
jpcchaves <jpcchaves@outlook.com>
2024-08-16 08:56:18 -0300

    add create pessoa fisica method

--- src/main/java/com/jpcchaves/ecommerce/service/PessoaService.java
+++ src/main/java/com/jpcchaves/ecommerce/service/PessoaService.java
@@ -1,8 +1,11 @@
 package com.jpcchaves.ecommerce.service;
 
+import com.jpcchaves.ecommerce.model.PessoaFisica;
 import com.jpcchaves.ecommerce.model.PessoaJuridica;
 
 public interface PessoaService {
 
   PessoaJuridica salvarPJ(PessoaJuridica pessoaJuridica);
+
+  PessoaFisica salvarPF(PessoaFisica pessoaFisica);
 }
--- src/main/java/com/jpcchaves/ecommerce/service/impl/PessoaServiceImpl.java
+++ src/main/java/com/jpcchaves/ecommerce/service/impl/PessoaServiceImpl.java
@@ -1,6 +1,7 @@
 package com.jpcchaves.ecommerce.service.impl;
 
 import com.jpcchaves.ecommerce.exception.BadRequestException;
+import com.jpcchaves.ecommerce.model.PessoaFisica;
 import com.jpcchaves.ecommerce.model.PessoaJuridica;
 import com.jpcchaves.ecommerce.model.Usuario;
 import com.jpcchaves.ecommerce.repository.PessoaRepository;
@@ -8,6 +9,7 @@ import com.jpcchaves.ecommerce.repository.UsuarioRepository;
 import com.jpcchaves.ecommerce.service.EmailService;
 import com.jpcchaves.ecommerce.service.PessoaService;
 import com.jpcchaves.ecommerce.utils.CnpjValidator;
+import com.jpcchaves.ecommerce.utils.CpfValidator;
 import com.jpcchaves.ecommerce.utils.EmailTemplateUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -93,8 +95,8 @@ public class PessoaServiceImpl implements PessoaService {
 
       usuarioPJ = usuarioRepository.save(usuarioPJ);
 
-      usuarioRepository.insereAcessoUserPj(usuarioPJ.getId(), "ROLE_USER");
-      usuarioRepository.insereAcessoUserPj(usuarioPJ.getId(), "ROLE_ADMIN");
+      usuarioRepository.insereAcessoUser(usuarioPJ.getId(), "ROLE_USER");
+      usuarioRepository.insereAcessoUser(usuarioPJ.getId(), "ROLE_ADMIN");
     }
 
     try {
@@ -112,5 +114,74 @@ public class PessoaServiceImpl implements PessoaService {
     }
 
     return pessoaJuridica;
+  }
+
+  @Override
+  public PessoaFisica salvarPF(PessoaFisica pessoaFisica) {
+
+    if (Objects.isNull(pessoaFisica)) {
+
+      throw new BadRequestException("Pessoa juridica nao pode ser nulo.");
+    }
+
+    if (Objects.isNull(pessoaFisica.getId())
+        && pessoaRepository.existsByCpf(pessoaFisica.getCpf()) != null) {
+
+      throw new BadRequestException(
+          "Ja existe uma pessoa juridica cadastrada" + " com o CNPJ informado");
+    }
+
+    if (!CpfValidator.isValid(pessoaFisica.getCpf())) {
+
+      throw new BadRequestException("CPF invalido!");
+    }
+
+    for (int i = 0; i < pessoaFisica.getEnderecos().size(); i++) {
+
+      pessoaFisica.getEnderecos().get(i).setPessoa(pessoaFisica);
+
+      pessoaFisica.getEnderecos().get(i).setEmpresa(pessoaFisica);
+    }
+
+    pessoaFisica = pessoaRepository.save(pessoaFisica);
+
+    Usuario usuarioPF =
+        usuarioRepository.findByPessoa(pessoaFisica.getId(), pessoaFisica.getEmail());
+
+    String senha = String.valueOf(Calendar.getInstance().getTimeInMillis());
+
+    if (Objects.isNull(usuarioPF)) {
+
+      usuarioPF = new Usuario();
+
+      usuarioPF.setDataAtualSenha(new Date());
+      usuarioPF.setEmpresa(pessoaFisica);
+      usuarioPF.setPessoa(pessoaFisica);
+      usuarioPF.setLogin(pessoaFisica.getEmail());
+
+      String senhaCriptografada = new BCryptPasswordEncoder(8).encode(senha);
+
+      usuarioPF.setSenha(senhaCriptografada);
+
+      usuarioPF = usuarioRepository.save(usuarioPF);
+
+      usuarioRepository.insereAcessoUser(usuarioPF.getId(), "ROLE_USER");
+    }
+
+    try {
+
+      emailService.send(
+          "Acesso para loja virtual gerado",
+          EmailTemplateUtil.buildConfirmationEmail(
+              pessoaFisica.getNome(), pessoaFisica.getEmail(), senha),
+          pessoaFisica.getEmail());
+
+    } catch (MessagingException | UnsupportedEncodingException ex) {
+
+      logger.error("An error occurred while sending the register email!");
+      logger.error(ex.getMessage());
+    }
+
+    return pessoaFisica;
   }
 }
