e77bb0dff98905f8e1b9d393076b21307e215f84
jpcchaves <jpcchaves@outlook.com>
2024-08-17 08:03:23 -0300

    add pessoa validation

--- src/main/java/com/jpcchaves/ecommerce/model/PessoaFisica.java
+++ src/main/java/com/jpcchaves/ecommerce/model/PessoaFisica.java
@@ -1,5 +1,7 @@
 package com.jpcchaves.ecommerce.model;
 
+import org.hibernate.validator.constraints.br.CPF;
+
 import java.io.Serializable;
 import java.util.Date;
 import javax.persistence.*;
@@ -13,6 +15,7 @@ import javax.persistence.*;
 public class PessoaFisica extends Pessoa implements Serializable {
   private static final long serialVersionUID = 1988296739060279654L;
 
+  @CPF(message = "CPF invalido")
   @Column(nullable = false)
   private String cpf;
 
